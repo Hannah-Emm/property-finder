ed3cc926284edf15131bd57a18089563d297d0a1
Hannah Molineaux <hannah.molineaux@gmail.com>
2026-02-23 20:43:52 +0000

    Extract time, change and fare details for journeys

--- journey-planner/journey.py
+++ journey-planner/journey.py
@@ -4,11 +4,19 @@ import psycopg
 import requests
 import json
 from datetime import datetime, timedelta
+from statistics import mean
+import re
+
+
+class JourneyDirection(Enum):
+    OUTBOUND = 0
+    RETURN = 1
 
 
 class StartType(Enum):
     DEPART = 0
     ARRIVE = 1
+    NONE = 2
 
 
 class DayOfWeek(Enum):
@@ -28,8 +36,8 @@ class TrainJourneySearchRequest():
             destination: str,
             start_time: str,
             start_type: StartType,
-            return_time: str,
-            return_type: StartType,
+            return_time: str | None,
+            return_type: StartType | None,
             day_of_week: DayOfWeek,
             rail_card: str):
         self.origin = origin
@@ -41,6 +49,76 @@ class TrainJourneySearchRequest():
         self.day_of_week = day_of_week
         self.rail_card = rail_card
 
+    def is_return_journey(self):
+        return self.return_time != None
+
+
+class JourneyTimeDetails():
+    def __init__(
+            self,
+            fastest_time: int,
+            average_time: int,
+            slowest_time: int,
+            least_changes: int,
+            most_changes: int,
+            shortest_wait: int | None = None,
+            average_wait: int | None = None,
+            longest_wait: int | None = None):
+        self.fastest_time = fastest_time
+        self.average_time = average_time
+        self.slowest_time = slowest_time
+        self.least_changes = least_changes
+        self.most_changes = most_changes
+        self.shortest_wait = shortest_wait
+        self.average_wait = average_wait
+        self.longest_wait = longest_wait
+
+    def __repr__(self):
+        return f"fastest_time={self.fastest_time} average_time={self.average_time} slowest_time={self.average_time} least_changes={self.least_changes} most_changes={self.most_changes} shortest_wait={self.shortest_wait} average_wait={self.average_wait} longest_wait={self.longest_wait}"
+
+
+class Fare():
+    def __init__(self, price: int, type: str, direction: str):
+        self.price = price
+        self.type = type
+        self.direction = direction
+
+    def __lt__(self, other):
+        return self.price < other.price
+
+    def __gt__(self, other):
+        return self.price > other.price
+
+    def __repr__(self):
+        return f"price={self.price} type={self.type} direction={self.direction}"
+
+
+class JourneyFareDetails():
+    def __init__(self, cheapest_return: Fare | None, cheapest_single: list[Fare] | None):
+        self.cheapest_return = cheapest_return
+        self.cheapest_single = cheapest_single
+
+    def __repr__(self):
+        return f"cheapest_return={self.cheapest_return}\ncheapest_single={self.cheapest_single}"
+
+
+class JourneyDetails():
+    def __init__(self, journey_time_details: JourneyTimeDetails):
+        self.journey_time_details = journey_time_details
+
+    def __repr__(self):
+        return f"times={self.journey_time_details}"
+
+
+class JourneySummary():
+    def __init__(self, outbound_details: JourneyDetails, fare_details: JourneyFareDetails, return_details: JourneyDetails | None = None, ):
+        self.outbound_details = outbound_details
+        self.fare_details = fare_details
+        self.return_details = return_details
+
+    def __repr__(self):
+        return f"outbound=({self.outbound_details})\nreturn=({self.return_details})\nfares={self.fare_details}"
+
 
 class TrainJourneySearchResponse():
     request: TrainJourneySearchRequest
@@ -51,14 +129,98 @@ class TrainJourneySearchResponse():
         self.request = request
         self.checked_at = checked_at
         self.data = data
+        self._journey_summary = None
+
+    def get_journey_summary(self) -> JourneySummary:
+        if self._journey_summary == None:
+            self._journey_suummary = JourneySummary(
+                self._get_journey_time_details(JourneyDirection.OUTBOUND),
+                self._get_journey_fare_details(),
+                None if self.request.return_time == None else self._get_journey_time_details(
+                    JourneyDirection.RETURN)
+            )
+        return self._journey_suummary
+
+    def _get_journey_time_details(self, direction: JourneyDirection) -> JourneyTimeDetails:
+        durations = []
+        times = []
+        min_changes = None
+        max_changes = None
+        for journey in self.data["outwardJourneys" if direction == JourneyDirection.OUTBOUND else "inwardJourneys"]:
+            duration = 0
+            for durationPart in journey["duration"].split(" "):
+                durationNumber = int(re.match(r'\d+', durationPart)[0])
+                if durationPart.endswith("h"):
+                    duration += durationNumber * 60
+                elif durationPart.endswith("m"):
+                    duration += durationNumber
+            durations.append(duration)
+
+            times.append(datetime.strptime(
+                journey["timetable"]["scheduled"]["departure"], "%Y-%m-%dT%H:%M:%SZ"))
+
+            changes = len(journey["legs"])
+            if min_changes == None:
+                min_changes = changes
+                max_changes = changes
+            else:
+                min_changes = min(min_changes, changes)
+                max_changes = max(max_changes, changes)
+
+        if len(times) > 1:
+            wait_times = []
+            for i in range(0, len(times)-1, 2):
+                wait_times.append(
+                    int((times[i+1] - times[i]).total_seconds() / 60))
+            return JourneyTimeDetails(
+                min(durations),
+                mean(durations),
+                max(durations),
+                min_changes,
+                max_changes,
+                min(wait_times),
+                mean(wait_times),
+                max(wait_times))
+        return JourneyTimeDetails(min(durations), mean(durations), max(durations), min_changes, max_changes)
+
+    def _get_journey_fare_details(self) -> JourneyFareDetails:
+        return_fares = []
+        outbound_single_fares = []
+        inbound_single_fares = []
+
+        for journey in (self.data["outwardJourneys"] + ([] if not self.request.is_return_journey() else self.data["inwardJourneys"])):
+            for fare in journey["fares"]:
+                fare_details = Fare(
+                    fare["totalPrice"], fare["typeDescription"], fare["direction"])
+                if fare_details.direction == "RETURN":
+                    return_fares.append(fare_details)
+                elif fare_details.direction == "OUTWARD":
+                    outbound_single_fares.append(fare_details)
+                elif fare_details.direction == "INWARD":
+                    inbound_single_fares.append(fare_details)
+
+        cheapest_return_fare = None
+        if len(return_fares) != 0:
+            return_fares.sort()
+            cheapest_return_fare = return_fares[0]
+
+        cheapest_single_fares = None
+        if len(outbound_single_fares) != 0:
+            outbound_single_fares.sort()
+            cheapest_single_fares = [outbound_single_fares[0]]
+            if len(inbound_single_fares) != 0:
+                inbound_single_fares.sort()
+                cheapest_single_fares.append(inbound_single_fares[0])
+
+        return JourneyFareDetails(cheapest_return_fare, cheapest_single_fares)
 
 
 class JourneyFinder():
-    ENDPOINT = "https://jpservices.nationalrail.co.uk/journey-planner"
-    HEADERS = {
+    _ENDPOINT = "https://jpservices.nationalrail.co.uk/journey-planner"
+    _HEADERS = {
         "Accept-Encoding": "gzip, deflate"
     }
-    DATE_FORMAT = "%Y-%m-%d"
+    _DATE_FORMAT = "%Y-%m-%d"
 
     def __init__(self, connection: psycopg.Connection):
         self.connection = connection
@@ -83,12 +245,13 @@ class JourneyFinder():
             current_week_day = today.weekday() + 1 if today.weekday() < 6 else 0
             if current_week_day < request.day_of_week.value:
                 search_date = (today + timedelta(days=request.day_of_week.value -
-                               current_week_day)).strftime(JourneyFinder.DATE_FORMAT)
+                               current_week_day)).strftime(JourneyFinder._DATE_FORMAT)
             else:
                 search_date = (today + timedelta(days=7 - current_week_day +
-                               request.day_of_week.value)).strftime(JourneyFinder.DATE_FORMAT)
+                               request.day_of_week.value)).strftime(JourneyFinder._DATE_FORMAT)
             outward_time = f"{search_date}T{request.start_time}Z"
-            inward_time = f"{search_date}T{request.return_time}Z"
+            if request.return_time != None:
+                inward_time = f"{search_date}T{request.return_time}Z"
             api_request_body = {
                 "origin": {"crs": request.origin, "group": False},
                 "destination": {"crs": request.destination, "group": True},
@@ -103,12 +266,13 @@ class JourneyFinder():
                 "onlySearchForSleeper": False,
                 "overtakenTrains": True,
                 "useAlternativeServices": False,
-                "inwardTime": {"travelTime": inward_time, "type": request.return_type.name},
                 "increasedInterchange": "ZERO"
             }
-
+            if request.return_time != None:
+                api_request_body["inwardTime"] = {
+                    "travelTime": inward_time, "type": request.return_type.name}
             api_response = requests.post(
-                JourneyFinder.ENDPOINT, json=api_request_body, headers=JourneyFinder.HEADERS)
+                JourneyFinder._ENDPOINT, json=api_request_body, headers=JourneyFinder._HEADERS)
             if api_response.status_code == 400:
                 print("Bad request for ", request)
                 return None
@@ -123,6 +287,6 @@ class JourneyFinder():
                 on conflict (origin, destination, start_time, start_type, return_time, return_type, day_of_week, rail_card)
                 do update set checked_at=now(), data=excluded.data
                 """,
-                               [request.origin, request.destination, request.start_time, request.start_type, request.return_time, request.return_type, request.day_of_week, request.rail_card, json.dumps(response.data)])
+                               [request.origin, request.destination, request.start_time, request.start_type, (request.return_time if request.return_time != None else "00:00:00"), (request.return_type if request.return_time != None else StartType.NONE), request.day_of_week, request.rail_card, json.dumps(response.data)])
                 self.connection.commit()
         return response
